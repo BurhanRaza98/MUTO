7691a48d2cdc182356e994b4c980b64207ebf319
Burhan Raza <burhanraza98@gmail.com>
2024-12-11 16:42:03 +0100

    Initial Commit

---
+++ MUTO/MUTOApp.swift
@@ -0,0 +1,17 @@
+//
+//  MUTOApp.swift
+//  MUTO
+//
+//  Created by Burhan Raza on 11/12/24.
+//
+
+import SwiftUI
+
+@main
+struct MUTOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
